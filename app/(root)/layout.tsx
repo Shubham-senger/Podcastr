7ff37ba52d0b12ca65ab8c6859f35e6a1f7aed43
Shubham Sengar <shubhamsengar2303@gmail.com>
2025-01-02 20:27:04 +0530

    Base structure is ready

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -7,11 +10,30 @@ export default function RootLayout({
 }>) {
   return (
     <div className="relative flex flex-col">
-        <main className="relative flex bg-black-3">
-            <LeftSidebar/>
-            {children}
-            <p className="text-white-1">RIGHT SIDEBAR</p>
-        </main>
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+
+        <section className=" flex min-h-screen flex-1 flex-col p-4 sm:p-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src="/icons/logo.svg"
+                alt="menu logo"
+                width={30}
+                height={30}
+              />
+              <MobileNav/>
+            </div>
+
+            <div className="flex flex-col md:pb-14">
+              Toaster 
+              {children}
+            </div>
+          </div>
+        </section>
+
+        <RightSidebar />
+      </main>
     </div>
   );
 }
