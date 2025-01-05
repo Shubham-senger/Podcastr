4f85ef29ed6947e76cec10fbd167315fcd6125b6
Shubham Sengar <shubhamsengar2303@gmail.com>
2025-01-06 01:18:48 +0530

    create podcast page completed

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,10 +1,14 @@
-const authConfig = {
-  providers: [
-    {
-      domain: "https://talented-snapper-10.clerk.accounts.dev",
-      applicationID: "convex",
-    },
-  ],
-};
 
-export default authConfig;
+
+const authConfig ={
+    providers: [
+      {
+        domain: "https://talented-snapper-10.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
+
+
